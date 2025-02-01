eb8820c8a82ccdd0ccf7dcd5c922d6c11ab5add0
celio ROCHA <celio.mozes.rocha@gmail.com>
2025-02-01 22:10:18 +0100

    update demo avec modal

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,13 +1,13 @@
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 import {
-	MapContainer,
-	TileLayer,
-	Marker,
-	Popup,
-	LayersControl,
-	LayerGroup,
-	MarkerProps,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  LayersControl,
+  LayerGroup,
+  MarkerProps,
 } from "react-leaflet";
 // Pour affiocher la modale
 import { useCallback, useState } from "react";
@@ -25,35 +25,40 @@ import ModalStation from "./ModalStation";
 
 // markers
 const markers = [
-	{
-		geocode: [48.86, 2.3822],
-		popUp: "Hello, I am pop up 1",
-	},
-	{
-		geocode: [48.83, 2.3562],
-		popUp: "Hello, I am pop up 2",
-	},
-	{
-		geocode: [48.885, 2.34],
-		popUp: "Hello, I am pop up 3",
-	},
+  {
+    geocode: [48.86, 2.3822],
+    popUp: "FRA6575466",
+  },
+  {
+    geocode: [48.83, 2.3562],
+    popUp: "Hello, I am pop up 2",
+  },
+  {
+    geocode: [48.885, 2.34],
+    popUp: "Hello, I am pop up 3",
+  },
 ];
 
 // [48.8566, 2.3522]
 
 export default function Maps({
-	selectedPosition,
-}: { selectedPosition: searchApi | null }) {
-	const [showModal, setShowModal] = useState(false);
+  selectedPosition,
+}: {
+  selectedPosition: searchApi | null;
+}) {
+  const [showModal, setShowModal] = useState(false);
+  const [ident, setident] = useState("");
+  // detection du click sur un Marker
 
-	// detection du click sur un Marker
+  const handleClick = useCallback((e) => {
+    console.log("marker clicked", e.target.options.children.props.children);
+    setident(e.target.options.children.props.children);
+    setShowModal(true);
+  }, []);
 
-	const handleClick = useCallback((e) => {
-		console.log("marker clicked", e);
-		setShowModal(true);
-	}, []);
+  console.info("ident", ident);
 
-	/* const startPoint = {
+  /* const startPoint = {
 		lat: 48.86,
 		lng: 2.3822,
 	};
@@ -68,53 +73,56 @@ export default function Maps({
 
 	const endPoint = { lat: 48.86, lng: 2.3822 };
  */
-	return (
-		<>
-			<MapContainer center={[48.8566, 2.3522]} zoom={13} zoomControl={false}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				<LayersControl position="topleft">
-					<LayersControl.Overlay name="Power Charge">
-						<Marker
-							position={markers[1].geocode}
-							eventHandlers={{
-								click: (e) => {
-									console.log(`Click sur le Marker ${e.latlng}`);
-								},
-							}}
-						>
-							<Popup>{markers[1].popUp}</Popup>
-						</Marker>
-					</LayersControl.Overlay>
-					<MarkerClusterGroup chunkedLoading>
-						<Marker
-							position={markers[0].geocode}
-							eventHandlers={{ click: handleClick }}
-						>
-							<Popup>{markers[0].popUp}</Popup>
-						</Marker>
+  return (
+    <>
+      <MapContainer center={[48.8566, 2.3522]} zoom={13} zoomControl={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LayersControl position="topleft">
+          <LayersControl.Overlay name="Power Charge">
+            <Marker
+              position={markers[1].geocode}
+              eventHandlers={{
+                click: (e) => {
+                  console.log(`Click sur le Marker ${e.latlng}`);
+                },
+              }}
+            >
+              <Popup>{markers[1].popUp}</Popup>
+            </Marker>
+          </LayersControl.Overlay>
+          <MarkerClusterGroup chunkedLoading>
+            <Marker
+              position={markers[0].geocode}
+              eventHandlers={{ click: handleClick }}
+            >
+              <Popup>{markers[0].popUp}</Popup>
+            </Marker>
 
-						<Marker position={markers[2].geocode}>
-							<Popup>{markers[2].popUp}</Popup>
-						</Marker>
-					</MarkerClusterGroup>
-					<LayersControl.Overlay name="Ma position" checked={true}>
-						<LayerGroup>
-							<LocationUser selectedPosition={selectedPosition} />
-						</LayerGroup>
-					</LayersControl.Overlay>
-				</LayersControl>
-				{/* <Routing start={startPoint} end={endPoint} /> */}
+            <Marker position={markers[2].geocode}>
+              <Popup>{markers[2].popUp}</Popup>
+            </Marker>
+          </MarkerClusterGroup>
+          <LayersControl.Overlay name="Ma position" checked={true}>
+            <LayerGroup>
+              <LocationUser selectedPosition={selectedPosition} />
+            </LayerGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
+        {/* <Routing start={startPoint} end={endPoint} /> */}
 
-				{/*ajout de la modal */}
-				{showModal &&
-					createPortal(
-						<ModalStation onClose={() => setShowModal(false)} />,
-						document.body,
-					)}
-			</MapContainer>
-		</>
-	);
+        {/*ajout de la modal */}
+        {showModal &&
+          createPortal(
+            <ModalStation
+              onClose={() => setShowModal(false)}
+              stationId={ident}
+            />,
+            document.body
+          )}
+      </MapContainer>
+    </>
+  );
 }
--- src/components/ModalStation.tsx
+++ src/components/ModalStation.tsx
@@ -1,8 +1,20 @@
-export default function ModalStation({ onClose }) {
-	return (
-		<div className="modal">
-			<div>Voici la postion </div>
-			<button onClick={onClose}>Close</button>
-		</div>
-	);
+import { MouseEventHandler } from "react";
+
+export default function ModalStation({
+  onClose,
+  stationId,
+}: {
+  onClose: MouseEventHandler;
+  stationId: string;
+}) {
+  return (
+    <div className="modal">
+      <div>
+        <h1>Voici la postion</h1>{" "}
+      </div>
+      Id de la sation : {stationId}
+      <button onClick={onClose}>Close</button>
+      {}
+    </div>
+  );
 }
